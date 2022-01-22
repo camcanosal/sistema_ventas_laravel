9871313b5d63614a3fe6f4bc0a0d1ca5c17d4599
camcanosal <camcanosal@unal.edu.co>
2022-01-22 15:33:41 -0500

    Se agrega proyecto

---
+++ app/Venta.php
@@ -0,0 +1,19 @@
+
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venta extends Model
+{
+    public function productos()
+    {
+        return $this->hasMany("App\ProductoVendido", "id_venta");
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo("App\Cliente", "id_cliente");
+    }
+}
